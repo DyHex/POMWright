55fa1d67deffda88d92f0ca989541d0f4ee8ff60
DyHex <maggmadon@gmail.com>
2024-02-15 11:24:27 +0100

    Update VSCode extensions and ignore files in biome.json

--- example/tests/saucedemo/login/error/userLockOut.spec.ts
+++ example/tests/saucedemo/login/error/userLockOut.spec.ts
@@ -1,5 +1,5 @@
-import { test } from "../../../../fixtures/all.fixtures";
 import { expect } from "@playwright/test";
+import { test } from "../../../../fixtures/all.fixtures";
 
 test("locked-out user should not be able to login", async ({ sdHome }) => {
 	await sdHome.page.goto(sdHome.fullUrl);
