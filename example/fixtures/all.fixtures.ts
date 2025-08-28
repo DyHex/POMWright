091f52b09be0244b1eae436501c018842fe45878
Magnus Blütecher Dysthe <maggmadon@gmail.com>
2025-08-28 14:49:05 +0200

    chore: remove the old example folder
    Better and uptodate examples in intTest folder

--- example/fixtures/all.fixtures.ts
+++
@@ -1,4 +0,0 @@
-import { mergeTests } from "@playwright/test";
-import { test as saucedemoTest } from "../page-object-models/saucedemo/fixtures/saucedemo.fixtures";
-
-export const test = mergeTests(saucedemoTest);
